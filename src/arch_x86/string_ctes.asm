3b297e395acb70896c614de54ec05b6ba022abc4
Pierre <pier.engelstein@outlook.com>
2018-04-07 22:23:37 +0200

    Added / Deleted some strings constants
    Deleted the hex_out string as it is useless in the new print_number method. Added a total_size string in second_stage_messages section. Added a byte message in general section.

--- src/arch_x86/string_ctes.asm
+++ src/arch_x86/string_ctes.asm
@@ -9,12 +9,13 @@
 s_startupSecond    db "Started the second stage bootloader!",10 , 13, 0
 s_stage_start      db "Second stage bootloader memory addresses : ", 10, 13, "Start : ", 0
 s_stage_end        db "End   : ", 0
+s_stage_size       db "Total size : ", 0
+
 ;-------------------------------------
 
 ;Error messages :
 ;-------------------------------------
 
 ;General messages :
-s_hex_out          db '0x0000', 10, 13, 0
-s_empty_line       db 10, 13, 0
+s_bytes            db " bytes ", 0
 ;-------------------------------------
