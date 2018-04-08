6c4f5c08f126f5edafe344277a00ec3282d4f58e
Pierre <pier.engelstein@outlook.com>
2018-04-08 19:33:06 +0200

    String constants modification
    Added the strings needed for the protected mode switch
    Added KiB and MiB size indicator string

--- src/arch_x86/string_ctes.asm
+++ src/arch_x86/string_ctes.asm
@@ -10,12 +10,18 @@ s_startupSecond    db "Started the second stage bootloader!",10 , 13, 0
 s_stage_start      db "Second stage bootloader memory addresses : ", 10, 13, "Start : ", 0
 s_stage_end        db "End   : ", 0
 s_stage_size       db "Total size : ", 0
-
+s_lowmem_size      db "Low memory size : ", 0
+s_a20_check        db "Checking A20 gate ...", 10, 13, 0
+s_a20_enabled      db "A20 gate enabled !", 10, 13, 0
+s_a20_disabled     db "A20 gate is disabled, jumping to enable it...", 10, 13, 0
 ;-------------------------------------
 
 ;Error messages :
+s_a20error         db "Failed to enable A20 gate ! Error code is [ah] : ", 0
 ;-------------------------------------
 
 ;General messages :
 s_bytes            db " bytes ", 0
+s_KiB              db " KiB ", 0
+s_MiB              db " MiB", 0
 ;-------------------------------------
