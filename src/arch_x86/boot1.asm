16be10350cc8f7d31b8c1752d88f262483de70e5
Pierre <pier.engelstein@outlook.com>
2018-04-14 12:49:56 +0200

    Disk load routine addition
    - Added the disk_load routine to the second stage bootloader that allows us to read Kernel and write it to memory (+error routine);
    - Changed a bit some messages printed on screen

--- src/arch_x86/boot1.asm
+++ src/arch_x86/boot1.asm
@@ -21,7 +21,10 @@ mov ss, ax
 sti                            ;Now that we are safe, we can re-enable the interrupts
 
 ;Now that's done, we can do whatever we want to.
-call clear_screen              ;We clear the screen from previous messages we did not needed
+;call clear_screen              ;We clear the screen from previous messages we did not needed
+;call goToLine
+mov bx, s_separator
+call print_str
 ;Some messages indicating information
 mov cx, 16
 mov bx, s_startupSecond
@@ -30,11 +33,13 @@ call goToLine
 mov bx, s_stage_start
 call print_str
 mov ax, $$
+push $$
 call print_number
 call goToLine
 mov bx, s_stage_end
 call print_str
 mov ax, $$ + SECOND_STAGE_SIZE*512
+push $$ + SECOND_STAGE_SIZE*512
 call print_number
 call goToLine
 mov cx, 10
@@ -50,7 +55,6 @@ int 0x12                      ;Get the low-memory size (KiB) with interrupt 12h 
 mov bx, s_lowmem_size         ;And print it
 call print_str
 mov cx, 10
-mov dx, ax
 call print_number
 mov bx, s_KiB
 call print_str
@@ -64,7 +68,6 @@ mov ax, 0x2402              ;Interrupt 15h, function 2402h : query A20 gate stat
 int 0x15
 cmp al, 0                  ;Result stored in AL : AL=0 => gate disabled ; AL=1 => gate enabled
 jne skipa20                ;If the gate is enabled, we skip the enabling of the gate (to avoid errors)
-
 mov bx, s_a20_disabled
 call print_str
 mov ax, 0x2401             ;Interrupt 15h, function 2401 : enable A20
@@ -73,8 +76,21 @@ jc errora20                ;Stop the program if there is an error (carry flag se
 skipa20:                   ;Else, we continue the road
 mov bx, s_a20_enabled
 call print_str
-
+call goToLine
 ;Load kernel into memory
+mov bx, s_kernel_read
+call print_str
+mov bx, KERNEL_LOCATION    ;Load the Kernel at 0x9000
+mov dh, KERNEL_SIZE        ;Read 2 sectors (for the moment it is static, will work to make it dynamic later)
+mov cl, 0x02               ;Start reading after this sector
+add cl, SECOND_STAGE_SIZE
+call disk_load
+
+mov bx, s_kernel_endread
+call print_str
+mov ax, KERNEL_LOCATION
+mov cx, 16
+call print_number
 
 ;Switch to protected mode
 call switch_to_pm
@@ -90,6 +106,39 @@ errora20:
    cli
    jmp $
 
+;Disk load routine
+;Call with :
+;              bx = Where to store the sectors read
+;              dh = How much sectors to read
+;              cl = From which sector we read [0x01 -> 0x11]
+;Return :
+; If error, ah = error code
+;           al = number of sector that have been already read
+;TODO : manage multiple head/cylinder if we want to load more than 128 sectors (which could be a good idea...)
+disk_load:
+  pusha
+  push dx
+  mov ah, 0x02      ;Interrupt 13h, function 02h
+  mov al, dh        ;How much sectors do we read [0x01 -> 0x80]
+  mov dh, 0x00      ;Head number
+  mov dl, 0x80      ;dl = 0x00 if floppy disk 1, 0x01 if floppy disk 2, 0x80 if hdd 1, 0x81 if hdd2
+  mov ch, 0x00      ;cylinder number [0x000 -> 0x3FF]
+  int 0x13
+  jc errorDiskLoad  ;Stop the program if there is an error (carry flag set)
+  pop dx
+  cmp al, dh
+  jne errorDiskLoad ;We want to be sure that we read all the sectors we wanted
+  popa
+  ret
+;Disk load error
+errorDiskLoad:
+   mov bx, s_diskreadError
+   call print_str
+   mov dx, ax
+   mov cx, 16
+   call print_number
+   cli
+   jmp $
 ;----------Includes needed----------
 %include "src/arch_x86/print.asm"
 %include "src/arch_x86/string_ctes.asm"
