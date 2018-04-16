5a10949ebde04f1060b8037e0c89d1ebf917b301
Pierre <pier.engelstein@outlook.com>
2018-04-16 19:32:55 +0200

    Assemble script update
    Added the kernel compilation to the assemble.sh script

--- assemble.sh
+++ assemble.sh
@@ -8,16 +8,49 @@
 #-------------------------------
 
 #!/bin/bash
-
+clear
+#Color codes
+blue=`tput setaf 6`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+echo "${reset}**********"
+echo "Operating system image complier. Made by Pierre Engelstein."
 echo "**********"
-echo "Bootloader image complier. Made by Pierre Engelstein."
-echo "**********"
-echo "[0] Checking directories integrity and program presence ..."
+echo "${yellow}[0] ${green}Checking directories integrity and program presence${reset} ..."
 mkdir -p bin/obj/
-echo "[1] Compiling first stage bootloader ... nasm src/arch_x86/boot0.asm -f bin -o bin/boot0.bin"
+#First step, build the bootloader
+echo "${yellow}[1] ${green}Compiling first stage bootloader${reset}                    ... ${blue}nasm src/arch_x86/boot0.asm -f bin -o bin/boot0.bin${reset}"
 nasm src/arch_x86/boot0.asm -f bin -o bin/boot0.bin
-echo "[2] Compiling first stage bootloader ... nasm src/arch_x86/boot1.asm -f bin -o bin/boot1.bin"
+echo "${yellow}[2] ${green}Compiling first stage bootloader${reset}                    ... ${blue}nasm src/arch_x86/boot1.asm -f bin -o bin/boot1.bin${reset}"
 nasm src/arch_x86/boot1.asm -f bin -o bin/boot1.bin
-echo "[3] Creating hard drive image        ... cat bin/boot0.bin bin/boot1.bin > bin/boot.bin"
-cat bin/boot0.bin bin/boot1.bin bin/boot1.bin > bin/boot.bin
+echo "${yellow}[3] ${green}Compiling kernel entry file${reset}                         ... ${blue}nasm src/kernel/entry.asm -f elf64 -o bin/entry.bin${reset}"
+nasm src/kernel/entry.asm -f elf64 -o bin/obj/entry.o
+#Now it gets funny ... Compiling all the .c source code into .o obj file
+echo "${yellow}[4] ${green}Compiling kernel source code${reset}"
+declare -a FILELIST
+declare -a OBJLIST
+OBJLIST+=("bin/obj/entry.o")
+OUTPUT_EXT=".o"
+OBJ_FILE="bin/obj/"
+for filename in src/kernel/*.c;
+do
+         #Construct the name of the output file : src/kernel/something.c converted into bin/obj/something.o
+         FILELIST+=("$filename")
+         FILE="${filename##*/}"
+         FILE="${FILE%.*}"
+         OBJ="$OBJ_FILE$FILE$OUTPUT_EXT"
+         #Add this element into the list of object files
+         OBJLIST+=("$OBJ")
+         #And compile it
+         echo "${blue}gcc -ffreestanding -Idirectory -Wall -c $filename -o $OBJ${reset}"
+         gcc -ffreestanding -Idirectory -Wall -c $filename -o $OBJ
+done
+
+#And now we can create the full bin image of the kernel
+echo "${yellow}[5] ${green}Creating kernel binary image${reset}"
+echo "${blue}ld -Ttext 0x1000 ${OBJLIST[@]} -o bin/kernel.bin --oformat binary${reset}"
+ld -Ttext 0x1000 ${OBJLIST[@]} -s -o bin/kernel.bin --oformat binary
+echo "${yellow}[3] ${green}Creating hard drive image${reset}                           ... ${blue}cat bin/boot0.bin bin/boot1.bin bin/kernel.bin > bin/boot.bin${reset}"
+cat bin/boot0.bin bin/boot1.bin bin/kernel.bin > bin/boot.bin
 echo "**********"
