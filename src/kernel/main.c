b9a91e410d6f236bc783fca21f68b966632bd166
Pierre <pier.engelstein@outlook.com>
2018-04-16 19:34:22 +0200

    Main kernel file addition
    Added the main kernel file

---
+++ src/kernel/main.c
@@ -0,0 +1,30 @@
+void write_str(const char *string)
+{
+  char *video  =(char*) 0xb8000;
+  string++;
+  while(*string != 0)
+  {
+    *video = *string;
+    video++;
+    string++;
+    *video++ = 0x0f;
+    video++;
+  }
+}
+
+int main()
+{
+  int i;
+  char *video = (char*) 0xb8000;
+  for(i = 0; i <= 80*25*2; i += 2)
+  {
+    *video = ' ';
+    video++;
+    *video = 0x0f;
+    video++;
+  }
+  char str[6] = "hello";
+  write_str(str);
+  while(1){};
+  return 1;
+}
