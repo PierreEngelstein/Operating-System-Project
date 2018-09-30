41ee16827c461795b6cea22067d40975d6ed9bf6
Pierre <pier.engelstein@outlook.com>
2018-09-30 17:45:08 +0200

    * Changed some compilation option * Added a print of kernel total size in bytes and in sectors

--- assemble.sh
+++ assemble.sh
@@ -3,7 +3,6 @@
 #This script goes through the different bootloader's stages .asm files, assembling them
 #and creating the main bootloader image.
 #-------------------------------
-
 #!/bin/bash
 clear
 #Color codes
@@ -21,7 +20,7 @@ echo "${yellow}[1] ${green}Compiling first stage bootloader${reset}             
 nasm src/arch_x86/boot0.asm -f bin -o bin/boot0.bin
 echo "${yellow}[2] ${green}Compiling first stage bootloader${reset}                    ... ${blue}nasm src/arch_x86/boot1.asm -f bin -o bin/boot1.bin${reset}"
 nasm src/arch_x86/boot1.asm -f bin -o bin/boot1.bin
-echo "${yellow}[3] ${green}Compiling kernel entry file${reset}                         ... ${blue}nasm src/kernel/entry.asm -f elf64 -o bin/entry.bin${reset}"
+echo "${yellow}[3] ${green}Compiling kernel entry file${reset}                         ... ${blue}nasm src/kernel/entry.asm -f elf32 -o bin/obj/entry.o${reset}"
 nasm src/kernel/entry.asm -f elf32 -o bin/obj/entry.o
 #Now it gets funny ... Compiling all the .c source code into .o obj file
 echo "${yellow}[4] ${green}Compiling kernel source code${reset}"
@@ -40,15 +39,19 @@ do
          #Add this element into the list of object files
          OBJLIST+=("$OBJ")
          #And compile it
-         echo "${blue}gcc -ffreestanding -Idirectory -Wall -c $filename -o $OBJ${reset}"
-         gcc -ffreestanding -fno-pie -m32 -Idirectory -Wall -c $filename -o $OBJ
+         echo "${blue}gcc -ffreestanding -fno-pie -m32 -Isrc/includes -Wall -c $filename -o $OBJ{reset}"
+         gcc -ffreestanding -m32 -Isrc/includes -Wall -c $filename -o $OBJ
          #clang -S -O3 -fsyntax-only -ffreestanding -fsyntax-only -c $filename -o $OBJ
 done
 
 #And now we can create the full bin image of the kernel
 echo "${yellow}[5] ${green}Creating kernel binary image${reset}"
-echo "${blue}ld -Ttext 0x1000 ${OBJLIST[@]} -o bin/kernel.bin --oformat binary${reset}"
+echo "${blue}ld -Ttext 0x9000 ${OBJLIST[@]} -m elf_i386 -s -o bin/kernel.bin --oformat binary${reset}"
 ld -Ttext 0x9000 ${OBJLIST[@]} -m elf_i386 -s -o bin/kernel.bin --oformat binary
+ker_size=$(stat --printf="%s\n" bin/kernel.bin)
+sec_size=512
+ker_sect=$((ker_size%512?ker_size/512+1:ker_size/512))
+echo "${green}Kernel Size = $ker_size bytes => $ker_sect sectors to read"
 echo "${yellow}[6] ${green}Creating hard drive image${reset}                           ... ${blue}cat bin/boot0.bin bin/boot1.bin bin/kernel.bin > bin/boot.bin${reset}"
 cat bin/boot0.bin bin/boot1.bin bin/kernel.bin > bin/boot.bin
 echo "**********"
