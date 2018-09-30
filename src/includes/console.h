9452dd2e4b2a7135337713658b678a551d5a0739
Pierre <pier.engelstein@outlook.com>
2018-09-30 17:50:49 +0200

    Console driver header update

---
+++ src/includes/console.h
@@ -0,0 +1,10 @@
+/* Console default size */
+static const int VGA_WIDTH  = 80;
+static const int VGA_HEIGHT = 25;
+/* Video memory start address (NOT AN ARRAY)*/
+static const int VGA_MEM = 0xb8000;
+/* Current cursor position */
+static int curr_row;
+static int curr_col;
+/* Current printing color */
+static int color;
