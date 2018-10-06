2d436c4c8e6535e1d77b25166e624d56108f3437
Pierre <pier.engelstein@outlook.com>
2018-10-06 10:34:56 +0200

    init_pics function addition

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,6 +1,13 @@
 #include "stdlib.h"
 #include "io.h"
 #include "console.h"
+
+#define PIC1 0x20
+#define PIC2 0xA0
+#define ICW1 0x11
+#define ICW4 0x01
+
+void clear();
 /** Clears the all screen **/
 void clear()
 {
@@ -15,16 +22,35 @@ void clear()
     }
 }
 
+void init_pics(int pic1, int pic2)
+{
+    /* Send ICW1 */
+    port_byte_out(PIC1, ICW1);
+    port_byte_out(PIC2, ICW1);
+    /* Send ICW2 */
+    port_byte_out(PIC1 + 1, pic1);
+    port_byte_out(PIC2 + 1, pic2);
+    /* Send ICW3 */
+    port_byte_out(PIC1 + 1, 4);
+    port_byte_out(PIC2 + 1, 2);
+    /* Send ICW4 */
+    port_byte_out(PIC1 + 1, ICW4);
+    port_byte_out(PIC2 + 1, ICW4);
+    /* disable all irqs */
+    port_byte_out(PIC1 + 1, 0xFF);
+}
 
 /* Initializes the console */
 void init_term()
 {
     curr_row = 0;
     curr_col = 0;
+    //clear();
 }
 
 int main()
 {
+    init_pics(0x20, 0x28);
     clear();
     kprintf("Started Kernel ...\n");
     kprintf("Initialising Kernel ...");
@@ -32,8 +58,15 @@ int main()
     kprintf("Finished!\n");
     int a = 123;
     kprintf("a = %d\n", a);
-    int b = port_byte_in(10);
-    kprintf("port 10 = %d", b);
+    char oldkey;
+char key;
+for (;;) {
+  oldkey=key;
+  key=port_byte_in(0x60);
+  if(!(oldkey==key)){
+    kprintf(key);
+  }
+}
     while(1){};
     return 1;
 }
