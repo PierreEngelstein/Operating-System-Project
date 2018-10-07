e2bf39aeddbb277da43c215b574519cda1b2999d
Pierre <pier.engelstein@outlook.com>
2018-10-07 17:50:03 +0200

    Added some constants

---
+++ src/includes/const.h
@@ -0,0 +1,12 @@
+
+#define PRIVATE static   /* Limits the scope of the function to the file where it is defined */
+#define PUBLIC
+/* Boolean */
+#define TRUE 1           /* Manage boolean with integers */
+#define FALSE 0          /* Manage boolean with integers */
+
+#define NULL ((void*)0)
+
+/* Macros */
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
