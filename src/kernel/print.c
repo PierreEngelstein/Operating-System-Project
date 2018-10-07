1c05e26c260fd6d78daea9889b299b3f1532fd8b
Pierre <pier.engelstein@outlook.com>
2018-10-07 17:50:28 +0200

    Formated the code

--- src/kernel/print.c
+++ src/kernel/print.c
@@ -5,99 +5,98 @@
 
 void kprintf(const char *string, ...)
 {
-    /* First step : get all the optional parameters (using stdarg header) */
-    va_list parameters;
-    va_start(parameters, string);
-    int i = 0;
-    char c;
-    /* All type fields */
-    int d;
-    while(string[i] != 0) /* Write all string until end_of_string character '\0' */
-    {
-        if(string[i] == '\n') /* New line => increase current row position */
+        /* First step : get all the optional parameters (using stdarg header) */
+        va_list parameters;
+        va_start(parameters, string);
+        int i = 0;
+        /* All type fields */
+        int d;
+        while(string[i] != 0) /* Write all string until end_of_string character '\0' */
         {
-            curr_row++;
-            curr_col = 0;
-            i++;
-        }else
-        {
-            /* Parse type fields, TODO : Escape sequences */
-            if(string[i] == '%')
-            {
-                i += 2;
-                switch (string[i-1]) {
-                    case 'd':       /* Output an int */
-                        d=va_arg(parameters, signed int);
-                        char *buffd;
-                        itoa(d, buffd, 10);
-                        kprintf(buffd);
-                        break;
-                    case 'x':       /* Output an hexadecimal */
-                        d=va_arg(parameters, signed int);
-                        char *buffx;
-                        itoa(d, buffx, 16);
-                        kprintf(buffx);
-                        break;
-                    case 'c':       /* Output an hexadecimal */
-                        d = va_arg(parameters, int);
-                        write_char(d, curr_col, curr_row);
+                if(string[i] == '\n') /* New line => increase current row position */
+                {
+                        curr_row++;
+                        curr_col = 0;
                         i++;
-                        curr_col++;
-                        break;
-                    default:
-                      kprintf("$");
-                      break;
+                }else
+                {
+                        /* Parse type fields, TODO : Escape sequences */
+                        if(string[i] == '%')
+                        {
+                                i += 2;
+                                switch (string[i-1]) {
+                                case 'd': /* Output an int */
+                                        d=va_arg(parameters, signed int);
+                                        char *buffd;
+                                        itoa(d, buffd, 10);
+                                        kprintf(buffd);
+                                        break;
+                                case 'x': /* Output an hexadecimal */
+                                        d=va_arg(parameters, signed int);
+                                        char *buffx;
+                                        itoa(d, buffx, 16);
+                                        kprintf(buffx);
+                                        break;
+                                case 'c': /* Output an hexadecimal */
+                                        d = va_arg(parameters, int);
+                                        write_char(d, curr_col, curr_row);
+                                        i++;
+                                        curr_col++;
+                                        break;
+                                default:
+                                        kprintf("$");
+                                        break;
+                                }
+                        }else
+                        {
+                                write_char(string[i], curr_col, curr_row);
+                                i++;
+                                curr_col++;
+                        }
+                        if(curr_col == VGA_WIDTH) /* If we are at the end of the line, we go to the next line */
+                        {
+                                curr_col = 0;
+                                curr_row++;
+                        }
                 }
-            }else
-            {
-                  write_char(string[i], curr_col, curr_row);
-                  i++;
-                  curr_col++;
-            }
-            if(curr_col == VGA_WIDTH) /* If we are at the end of the line, we go to the next line */
-            {
-                curr_col = 0;
-                curr_row++;
-            }
         }
-    }
-    update_cursor(curr_col, curr_row);
-    va_end(parameters);
+        update_cursor(curr_col, curr_row);
+        va_end(parameters);
 }
 
 void write_char(const char c, int col, int row)
 {
-    uint16_t index = (col + row * VGA_WIDTH) * 2; /* Transforms the (col, row) 2D coordinate to 1D index */
-    char *video = (char*) VGA_MEM;
-    video = video + index;
-    *video = c;
-    video++;
-    *video = 0x0f;
+        uint16_t index = (col + row * VGA_WIDTH) * 2; /* Transforms the (col, row) 2D coordinate to 1D index */
+        char *video = (char*) VGA_MEM;
+        video = video + index;
+        *video = c;
+        video++;
+        *video = 0x0f;
 }
 
 void update_cursor(int col, int row)
 {
-    uint16_t pos = row * VGA_WIDTH + col;
-    port_byte_out(REG_SCREEN_CTRL, 0x0F);
-    port_byte_out(REG_SCREEN_DATA, (uint8_t)(pos & 0xFF));
-    port_byte_out(REG_SCREEN_CTRL, 0x0E);
-    port_byte_out(REG_SCREEN_DATA, (uint8_t)((pos >> 8) & 0xFF));
+        uint16_t pos = row * VGA_WIDTH + col;
+        port_byte_out(REG_SCREEN_CTRL, 0x0F);
+        port_byte_out(REG_SCREEN_DATA, (uint8_t)(pos & 0xFF));
+        port_byte_out(REG_SCREEN_CTRL, 0x0E);
+        port_byte_out(REG_SCREEN_DATA, (uint8_t)((pos >> 8) & 0xFF));
 }
 
 int getCursorOffet()
 {
-  port_byte_out(REG_SCREEN_CTRL, 14);
-  int offset = port_byte_in(REG_SCREEN_DATA) << 8; /* High byte: << 8 */
-  port_byte_out(REG_SCREEN_CTRL, 15);
-  offset += port_byte_in(REG_SCREEN_DATA);
-return offset * 2; /* Position * size of character cell */
+        port_byte_out(REG_SCREEN_CTRL, 14);
+        int offset = port_byte_in(REG_SCREEN_DATA) << 8; /* High byte: << 8 */
+        port_byte_out(REG_SCREEN_CTRL, 15);
+        offset += port_byte_in(REG_SCREEN_DATA);
+        return offset * 2; /* Position * size of character cell */
 }
 
 int getOffsetRow(int offset)
 {
-    return  offset / (2 * VGA_WIDTH);
+        return offset / (2 * VGA_WIDTH);
 }
 int getOffsetCol(int offset)
 {
-    return (offset - (getOffsetRow(offset)*2*VGA_WIDTH))/2;
+        return (offset - (getOffsetRow(offset)*2*VGA_WIDTH))/2;
 }
