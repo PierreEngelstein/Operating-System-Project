4d39029b8f622f217b59842216dc9a86ed266fff
Pierre <pier.engelstein@outlook.com>
2018-10-14 16:08:19 +0200

    Circular buffer implementation check

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -59,24 +59,102 @@ int main()
         kprintf("Finished!\n");
         int a = 123;
         kprintf("a = %x\n", a);
-        int oldkey;
-        int key;
-        kbd_t keyb = initKeyboard();
-        /* prototype for inputing with keyboard */
-        for (;;)
+        int *buffer = (int*)0x100000;
+        int size = 10;
+        kprintf("=====Circular Buffer Check====\n");
+        cbuf_handle_t cbuf = circular_buff_init(buffer, size);
+        kprintf("Buffer initialized. Full = %d, empty = %d, size = %d\n",
+                circular_buff_full(cbuf),
+                circular_buff_empty(cbuf),
+                circular_buff_size(cbuf));
+        kprintf("******\nAdding %d values\n", size - 1);
+        for(int i = 0; i < (size - 1); i++)
         {
-                oldkey=key;
-                key=scan_keyboard();
-                // if((oldkey==key)) key = 0;
-                if(!(oldkey==key)) {
-                        // kprintf("hexa=%x\n", abs(key), abs(key));
-                        char c = keyToAscii(key, &keyb);
-                        if(c >= 32 && c <= 126 && c != 94)
-                                kprintf("%c\0", c);
-                        else if(c == 13)
-                                kprintf("\n");
-                }
+                circular_buff_put(cbuf, i);
+                // kprintf("Added %d, size now : %d\n", i, circular_buff_size(cbuf));
         }
+        kprintf("Full = %d, empty = %d, size = %d\n",
+                circular_buff_full(cbuf),
+                circular_buff_empty(cbuf),
+                circular_buff_size(cbuf));
+        kprintf("******\nReading back values:");
+        while(!circular_buff_empty(cbuf))
+        {
+                int data;
+                circular_buff_get(cbuf, &data);
+                kprintf("%d ", data);
+        }
+        kprintf("\n");
+        kprintf("******\nAdding %d values\n", size);
+        for(int i = 0; i < (size); i++)
+        {
+                circular_buff_put(cbuf, i);
+        }
+        kprintf("Full = %d, empty = %d, size = %d\n",
+                circular_buff_full(cbuf),
+                circular_buff_empty(cbuf),
+                circular_buff_size(cbuf));
+        kprintf("******\nReading back values: ");
+        while(!circular_buff_empty(cbuf))
+        {
+                int data;
+                circular_buff_get(cbuf, &data);
+                kprintf("%d ", data);
+        }
+        kprintf("\n");
+        kprintf("******\nAdding %d values\n", size + 5);
+        for(int i = 0; i < (size + 5); i++)
+        {
+                circular_buff_put(cbuf, i);
+        }
+        kprintf("Full = %d, empty = %d, size = %d\n",
+                circular_buff_full(cbuf),
+                circular_buff_empty(cbuf),
+                circular_buff_size(cbuf));
+        kprintf("******\nReading back values: ");
+        while(!circular_buff_empty(cbuf))
+        {
+                int data;
+                circular_buff_get(cbuf, &data);
+                kprintf("%d ", data);
+        }
+        kprintf("\n******\nAdding %d values using non-overwrite version\n", size + 5);
+        for(int i = 0; i < size + 5; i++)
+        {
+                circular_buff_put2(cbuf, i);
+                // kprintf("Added %d, size now : %d\n", i, circular_buff_size(cbuf));
+        }
+        kprintf("Full = %d, empty = %d, size = %d\n",
+                circular_buff_full(cbuf),
+                circular_buff_empty(cbuf),
+                circular_buff_size(cbuf));
+        kprintf("******\nReading back values: ");
+        while(!circular_buff_empty(cbuf))
+        {
+                int data;
+                circular_buff_get(cbuf, &data);
+                kprintf("%d ", data);
+        }
+        kprintf(" ... Check Finished");
+        // int oldkey;
+        // int key;
+        // kbd_t keyb = initKeyboard();
+        // /* prototype for inputing with keyboard */
+        // for (;;)
+        // {
+        //         oldkey=key;
+        //         key=kbd_interrupt(&keyb);
+        //         // if((oldkey==key)) key = 0;
+        //         if(!(oldkey==key)) {
+        //                 // kprintf("hexa=%x\n", abs(key), abs(key));
+        //                 printBuffer(&keyb);
+        //                 // char c = keyToAscii(key, &keyb);
+        //                 // if(c >= 32 && c <= 126 && c != 94)
+        //                 //         kprintf("%c\0", c);
+        //                 // else if(c == 13)
+        //                 //         kprintf("\n");
+        //         }
+        // }
         while(1) {};
         return 1;
 }
