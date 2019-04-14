8e89250597f7c292be9aafaa5165b011e0a93353
PierreEngelstein <pier.engelstein@outlook.com>
2019-04-14 11:27:54 +0200

    cleaned main.c

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -23,59 +23,17 @@ void init_pics(int pic1, int pic2)
         port_byte_out(PIC1 + 1, 0xFF);
 }
 
-char *fgets(char *dest, int max)
-{
-        int c;
-        char *p;
-        for(p = dest, max--; max > 0; max--)
-        {
-                c = scanf(); /* Process the input */
-                // c = getLastChar(); /* And get the last typed character */
-                if(c == '\n')
-                        break;
-                *p++ = c;
-        }
-        *p = '\0';
-        if(p == dest)
-                return NULL;
-        return (p);
-}
-
 int main()
 {
+        cli();
         init_pics(0x20, 0x27);
         idt_init();
         /* Initialize the console */
         console_init();
         /* Print some random thing on screen to test */
-        printf("Started Kernel ...\n");
-        printf("Testing number %d\n", 123);
-
-        printf("Start inputing things on keyboard :\n");
-        
-        // IDT_Entry *IDT;
-        // printf("\nsizeof(IDT_Entry) = %d\n", sizeof(unsigned char));
-        // IDT = 0x7000;
-        // extern int irq0();
-        // extern int irq2();
-        // unsigned long irq0_address = (unsigned long)irq0;
-        // unsigned long irq2_address = (unsigned long)irq2;
-        // createIDTEntry(&(IDT[47]), irq2_address);
-        // printf("address of IDT[0] : %x\n", &(IDT[47]));
-        // printf("IDT[0].offset_higherbits = %x\n", IDT[47].offset_higherbits);
-        // printf("IDT[0].offset_lowerbits = %x\n", IDT[47].offset_lowerbits);
-        // printf("IDT[0].selector = %x\n", IDT[47].selector);
-        // printf("IDT[0].type_attr = %x", IDT[47].type_attr);
-
-        /* Test the keyboard input */
-        // int lgMax = 128;
-        // char cmd[lgMax];
-        // cmd[0] = '\0';
-        // for(;;)
-        // {
-        //         fgets(cmd, lgMax);
-        // }
-        // printf("Hello World !");
+        printf("Starting OS ...\n");
+        printf(":\\>");
+        sei();
         for(;;){}
         return 0;
 }
