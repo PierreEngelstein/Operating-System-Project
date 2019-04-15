df5a500a19df5fc8a343b29d63734df26fd10b99
PierreEngelstein <pier.engelstein@outlook.com>
2019-04-15 11:30:43 +0200

    added types.h for common types redefinition

---
+++ src/includes/types.h
@@ -0,0 +1,29 @@
+/* Common types redefinition */
+
+#ifndef uint8
+typedef unsigned char uint8;
+#endif
+
+#ifndef int8
+typedef char int8;
+#endif
+
+#ifndef uint16
+typedef unsigned short uint16;
+#endif
+
+#ifndef int16
+typedef short int16;
+#endif
+
+#ifndef uint32
+typedef unsigned int uint32;
+#endif
+
+#ifndef int32
+typedef int int32;
+#endif
+
+#ifndef BOOL
+typedef uint8 BOOL;
+#endif
