cfdaa7bb8451174acc6576b94ac78ccdb45de932
PierreEngelstein <pier.engelstein@outlook.com>
2019-04-15 11:28:33 +0200

    trying stuff

--- src/kernel/entry.asm
+++ src/kernel/entry.asm
@@ -1,16 +1,16 @@
 [bits 32]
 [extern main]
 [global _start]
-;TODO : setup segments and stack
-; mov eax, 0x9000      ;Update the segment register
+; TODO : setup segments and stack
+mov eax, 0x9000      ;Update the segment register
 ; mov ds, eax
 ; mov ss, eax
 ; mov es, eax
 ; mov fs, eax
 ; mov gs, eax
-;
-; mov ebp, 0x80000     ;Setup the stack at the location we want
-; mov esp, ebp
+
+mov ebp, 0x80000     ;Setup the stack at the location we want
+mov esp, ebp
 
 _start:
    call main
