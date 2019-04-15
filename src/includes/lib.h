10738d8d81542943559cf9b54468703b1c9b2278
PierreEngelstein <pier.engelstein@outlook.com>
2019-04-15 11:31:59 +0200

    moved interrupt headers & cleared unused buffer headers

--- src/includes/lib.h
+++ src/includes/lib.h
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include "io.h"
 #include "maths.h"
+#include "drivers/interrupts.h"
 
 /** Some standard defines **/
 #ifndef NULL
@@ -14,9 +15,6 @@
 #ifndef TRUE
 #define TRUE 1
 #endif
-
-#undef BOOL
-typedef char BOOL;
 
 /* Convert an integer to a string */
 void itoa(int value, char* buffer, int base);
@@ -42,46 +40,35 @@ int getCursorOffet();
 int getOffsetRow(int offset);
 int getOffsetCol(int offset);
 
-// #define sizeof(x) ((&x + 1) - &x)
-
 void kpanic(char *message);
 #define assert(check, condition) do{ if ((check) != (condition)) kpanic();}while(0)
 
-/* Circular buffer */
-struct circular_buff_t{
-        int *buffer;
-        int head;
-        int tail;
-        int max;
-        int full;
-};
-typedef struct circular_buff_t circular_buff_t;
-typedef circular_buff_t *cbuf_handle_t;
-/* Circular Buffer Functions */
-cbuf_handle_t circular_buff_init(int* buffer, int size); /* Creates a circular buffer */
-// void circular_buff_free(cbuf_handle_t cbuf); /* Free a circular buffer (does not free data buffer) */
-void circular_buff_reset(cbuf_handle_t cbuf); /* Reset the circular buffer (head == tail) */
-void circular_buff_put(cbuf_handle_t cbuf, int data); /* Adds an element to the buffer (even if is full) */
-int circular_buff_put2(cbuf_handle_t cbuf, int data); /* Adds an element to the buffer if it is not full */
-int circular_buff_get(cbuf_handle_t cbuf, int *data); /* Retrieves an element from the buffer */
-int circular_buff_empty(cbuf_handle_t cbuf); /* Check if the circular buffer is empty */
-int circular_buff_full(cbuf_handle_t cbuf); /* Checks if the circular buffer is full */
-int circular_buff_capacity(cbuf_handle_t cbuf); /* Returns the max capacity of the buffer */
-int circular_buff_size(cbuf_handle_t cbuf); /* Returns the current number of elements in the buffer */
+Console tConsole; /* The current console used */
 
-/* Interrupts */
-typedef struct //8 bytes length
-{
-        unsigned short int offset_lowerbits; //Offset bits 0..15
-        unsigned short int selector; //A code segment selector in GDT or LDT
-        unsigned char zero; //Unused
-        unsigned char type_attr; //Type and attributes
-        unsigned short int offset_higherbits; //Offset bits 16..31
-}IDT_Entry;
-int idt_init();
-int createIDTEntry(IDT_Entry *entry, unsigned long irq_address);
-/* Disable all interrupts */
-void cli();
-/* Enable all interrupts */
-void sei();
-Console tConsole;
+/* Debug stuff */
+
+uint8_t ah;
+uint8_t al;
+uint8_t bh;
+uint8_t bl;
+uint8_t ch;
+uint8_t cl;
+uint8_t dh;
+uint8_t dl;
+
+uint16_t ax;
+uint16_t bx;
+uint16_t cx;
+uint16_t dx;
+
+uint32_t eax;
+uint32_t ebx;
+uint32_t ecx;
+uint32_t edx;
+uint32_t esi;
+uint32_t edi;
+uint32_t esp;
+uint32_t ebp;
+
+/* Prints the value of all registers to screen (dump of registers is done inside) */
+void print_registers();
