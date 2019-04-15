40c9efc0a158f5bd71b9e2b4b94443fd41eb0fd0
PierreEngelstein <pier.engelstein@outlook.com>
2019-04-15 11:27:48 +0200

    added pow() function

--- src/includes/maths.h
+++ src/includes/maths.h
@@ -1,1 +1,2 @@
 int abs(int n);
+int pow(int x, unsigned int y);
--- src/kernel/maths.c
+++ src/kernel/maths.c
@@ -5,8 +5,19 @@
 
 int abs(int n)
 {
-    if(n > 0)
-      return n;
-    else
-      return -n;
+        if(n > 0)
+                return n;
+        else
+                return -n;
+}
+
+int pow(int x, unsigned int y)
+{
+        int tmp;
+        if(y == 0)
+                return 1;
+        tmp = pow(x, y/2);
+        if(y % 2 == 0)
+                return tmp * tmp;
+        return x * tmp * tmp;
 }
