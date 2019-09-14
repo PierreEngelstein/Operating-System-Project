c7e7248e8abecdbde3fc183cd5c3e984f626614f
PierreEngelstein <pier.engelstein@outlook.com>
2019-09-14 19:04:56 +0200

    first attempt at gdt setup in kernel mode.

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,9 +1,12 @@
 #include "lib.h"
-
+#include "string.h"
+#include "k_memory.h"
 #define PIC1 0x20
 #define PIC2 0xA0
 #define ICW1 0x11
 #define ICW4 0x01
+
+// extern int gdt_descriptor;
 
 void init_pics(int pic1, int pic2)
 {
@@ -23,18 +26,140 @@ void init_pics(int pic1, int pic2)
         port_byte_out(PIC1 + 1, 0xFF);
 }
 
-int main()
+typedef struct gdt
 {
+        uint16 limit;
+        uint16 baseLo;
+        uint8  baseMid;
+        uint8 access;
+        uint8 granularity;
+        uint8  baseHi;
+}gdt;
+
+typedef struct gdtr
+{
+        uint16 m_limit;
+        uint32 m_base;
+}gdtr __attribute__((packed));
+
+// #define MAX_DESCRIPTORS 6
+
+// static struct gdt  *_gdt;
+// struct gdtr * _gdtr;
+
+// void *memset(void *dst, int c, size_t n)
+// {
+//         if(n)
+//         {
+//                 char *d = dst;
+//                 do
+//                 {
+//                         *d++ = c;
+//                 }while(--n);
+//         }
+//         return dst;
+// }
+
+// void printData(void *dst, size_t n)
+// {
+//         if(n)
+//         {
+//                 char *d = dst;
+//                 do
+//                 {
+//                         printf("%d;", *d);
+//                         *d++;
+//                 }while(--n);
+//                 printf("\n");
+//         }
+// }
+
+// extern void gdt_install();
+
+// void gdt_set_descriptor(uint32 i, uint64 base, uint64 limit, uint8 access, uint8 grand)
+// {
+//         if(i > MAX_DESCRIPTORS)
+//                 return;
+        
+//         memset((void *) &_gdt[i], 0, sizeof(gdt));
+
+//         /* Set limit and base addresses */
+//         _gdt[i].baseLo  = base & 0xffff;
+//         _gdt[i].baseMid = (base >> 16) & 0xff;
+//         _gdt[i].baseHi  = (base >> 24) & 0xff;
+//         _gdt[i].limit   = limit & 0xffff;
+
+//         /* Set flags and grandularity bytes */
+//         _gdt[i].access = access;
+//         _gdt[i].granularity = ((limit >> 16) & 0x0f);
+//         _gdt[i].granularity |= grand & 0xf0;
+// }
+
+// int gdt_init()
+// {
+//         _gdtr = (struct _gdtr *)_k_malloc(sizeof(struct gdtr));
+//         _gdt = (gdt *)_k_malloc(MAX_DESCRIPTORS * sizeof(gdt));
+        
+//         _gdtr->m_limit = (sizeof(struct gdt) * MAX_DESCRIPTORS) - 1;
+//         _gdtr->m_base  = (uint32) &_gdt[0];
+
+//         gdt_set_descriptor(0, 0, 0, 0, 0);
+//         gdt_set_descriptor(1, 0, 0xffffffff, 0x9A, 0xCF);
+//         printData((void *)&_gdt[1], sizeof(gdt));
+//         printf("\n%x %x\n", _gdt, _gdtr);
+//         printf("sizeof(gdt) = %d\n", sizeof(gdt));
+//         gdt_set_descriptor(2, 0, 0xffffffff, 0x92, 0xCF);
+//         gdt_set_descriptor(3, 0, 0xffffffff, 0xFA, 0xCF);
+//         gdt_set_descriptor(4, 0, 0xffffffff, 0xF2, 0xCF);
+
+//         gdt_install();
+//         return 0;
+// }
+
+int main(int a, int b)
+{
+        // struct gdtr *test = (struct gdtr *)a;
+
+        // struct gdt *test_gdt = (a->m_base - a->m_limit);
+        
         init_pics(0x20, 0x27);
-        /* Disable interrupts during initialisation */
+        // // /* Disable interrupts during initialisation */
+        _k_init_mem();
         cli();
+        
         idt_init();
+        
         /* Initialize the console */
         console_init();
+        
+        // gdt_init();
+        // int *array = _k_malloc(sizeof(int) * 256);
+        // printf("array at %x. Memory at %x\n", array, a);
+
+        // unsigned short total;
+        // unsigned char lowmem, highmem;
+        
+        // port_byte_out(0x70, 0x30);
+        // lowmem = port_byte_in(0x71);
+        // port_byte_out(0x70, 0x31);
+        // highmem = port_byte_in(0x71);
+
+        // total = lowmem | highmem << 8;
+        // printf("low = %d, high = %d, total = %d\n", lowmem, highmem, total);
+        // printf("args = [a:%x; b:%x]\n", a, b);
+        // printf("gdtr = [base:%x; limit:%x]\n", a->m_base, a->m_limit);
+        // printf("gdt = [limit:%x baseLo:%x; baseMid:%x; access:%x; granularity:%x; baseHi:%x]\n", test_gdt->limit, test_gdt->baseLo, test_gdt->baseMid, test_gdt->access, test_gdt->granularity, test_gdt->baseHi);
         printf("Starting OS ...\n");
+        #ifdef I386
+        printf("32 bit system !\n");
+        #elif defined X86_64
+        printf("64 bit system !\n");
+        #endif
+        printf("sizeof(long) = %d\n", sizeof(long));
         printf(">");
-        /* Enable interrupts we want */
+        // /* Enable interrupts we want */
         enable_interrupt(INTERRUPT_KEYBOARD);
+        // enable_interrupt(INTERRUPT_TIMER);
         for(;;){}
         return 0;
 }
