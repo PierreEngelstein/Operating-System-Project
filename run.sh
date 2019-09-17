59ba3a1adc548aee9cc1ecd092be97a6e3648454
PierreEngelstein <pier.engelstein@outlook.com>
2019-09-17 15:07:45 +0200

    Added comments for creating a vmware disk image

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-qemu-system-${1} -drive format=raw,file=os.bin -m 16M
+qemu-system-${1} -drive file=os.bin,media=disk,if=ide -m 512M
+
+# qemu-img convert -pO vmdk os.bin hard_disk.vmdk
+# vmware-vdiskmanager hard_disk.vmdk -x 10G
